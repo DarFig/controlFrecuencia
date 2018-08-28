2287c2d268e113832f6a8b2af4197d1d11682cf5
DarFig <log.Dariel@gmail.com>
2018-08-29 00:05:10 +0200

    upd obtenerUso

--- tools/obtenerUso.sh
+++ tools/obtenerUso.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}'| tail -n1 >> ./files/log.txt
+top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}'| tail -n1 >> ./files/log.txt
 
-top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }'  >> ./files/log.txt
+#top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }'  >> ./files/log.txt
+
+#ps aux |awk  '{print $3}' | tail -n+2 | paste -sd+ | bc | awk '{print $1/32}'
