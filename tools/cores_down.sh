2e843d3f9d23db1e9ca46dfdea2c5d6f651a1708
DarFig <log.Dariel@gmail.com>
2018-08-28 18:35:11 +0200

    upd core tools

--- tools/cores_down.sh
+++ tools/cores_down.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in {8..63}; do
-    echo 0 > /sys/devices/system/cpu/cpu"$i"/online > /dev/null
+    echo 0 > /sys/devices/system/cpu/cpu"$i"/online
 done
 
 echo "cores down" >> ./files/changes_log.txt
--- tools/cores_up.sh
+++ tools/cores_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in {8..63}; do
-    echo 1 > /sys/devices/system/cpu/cpu"$i"/online > /dev/null
+    echo 1 > /sys/devices/system/cpu/cpu"$i"/online
 done
 
 echo "cores up" >> ./files/changes_log.txt
