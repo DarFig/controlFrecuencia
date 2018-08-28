4d0bc8dfd2eb7380f8e953b1f3452b811144ca80
DarFig <log.Dariel@gmail.com>
2018-08-28 20:09:32 +0200

    bugfix cpuusage -> tools

--- tools/obtenerUso.sh
+++ tools/obtenerUso.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}'| tail -n1 >> ./files/log.txt
+#top -b -n2 | grep "Cpu(s)" | awk '{print $2+$4}'| tail -n1 >> ./files/log.txt
+
+top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }'  >> ./files/log.txt
